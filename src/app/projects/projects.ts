e090dc10ddc7eca184191d5db5b58c9a6d502f62
Andrew <andrew.lintereur@gmail.com>
2016-11-27 19:11:19 -0600

    Implement: Added new UI for projects page, updated UI on login page

--- src/app/projects/projects.ts
+++ src/app/projects/projects.ts
@@ -6,5 +6,17 @@ import {Component} from '@angular/core';
   templateUrl: './projects.html'
 })
 export class Projects {
-    projectList = ['{{ JSON Utility }}', 'test']
+    public highlighted = ''
+    public content = ''
+    projectList = ['{{ JSON Utility }}', 'test1', 'test2', 'test3']
+    projectContent = {
+        '{{ JSON Utility }}' : 'A utility for easily visualizing, manipulating, and querying for JSON content the REST calls'
+        'test1' : 'This is just test content',
+        'test2' : 'This is just test content',
+        'test3' : 'This is just test content',
+    }
+    showContent(project){
+        this.highlighted = project;
+        this.content = this.projectContent[project];
+    }
 }
