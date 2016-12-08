518d594bb51bcd014940c5622009e9e4ca22e169
Andrew <andrew.lintereur@gmail.com>
2016-12-07 22:03:27 -0600

    Implement: Fixed styling on projects page and got page to load based on static projects object

--- src/app/projects/projects.ts
+++ src/app/projects/projects.ts
@@ -10,13 +10,25 @@ export class Projects {
     public content = ''
     projectList = ['{{ JSON Utility }}', 'test1', 'test2', 'test3']
     projectContent = {
-        '{{ JSON Utility }}' : 'A utility for easily visualizing, manipulating, and querying for JSON content the REST calls',
-        'test1' : 'This is just test content',
-        'test2' : 'This is just test content',
-        'test3' : 'This is just test content',
+        '{{ JSON Utility }}' : {
+            description : 'A utility for easily visualizing, manipulating, and querying for JSON content the REST calls',
+            href : 'google.com'
+        },
+        'test1' : {
+            description : 'This is just test content',
+            href : 'google.com'
+        },
+        'test2' : {
+            description : 'This is just test content',
+            href : 'google.com'
+        },
+        'test3' : {
+            description : 'This is just test content',
+            href : 'google.com'
+        },
     }
-    showContent(project){
-        this.highlighted = project;
-        this.content = this.projectContent[project];
-    }
+//    showContent(project){
+//        this.highlighted = project;
+//        this.content = this.projectContent[project];
+//    }
 }
