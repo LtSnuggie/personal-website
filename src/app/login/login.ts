9935221e2ea1be70c27dc89c51d5a7f6c053d86d
Andrew <andrew.lintereur@gmail.com>
2017-01-02 18:00:12 -0600

    Fix-Partial: Start to convert login to form, using ng built-ins

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,14 +1,48 @@
 import {Component} from '@angular/core';
-import {MobileService} from '../components/services/service';
+import {NgForm} from '@angular/forms';
+//import {MobileService} from '../components/services/service';
 
 @Component({
   selector: 'login',
   styleUrls: ['./login.css'],
   templateUrl: './login.html'
 })
-export class Login extends MobileService {
-        
-    constructor(private mobileService: MobileService){
-        super(mobileService.ngZone);
+//export class Login extends MobileService {
+export class Login {
+    
+    username: string;
+    password: string;
+    invalid : boolean;
+    f : NgForm;
+    attemptMap = {};
+    lockout: boolean;
+    
+//    constructor(private mobileService: MobileService){
+    constructor(){
+//        super(mobileService.ngZone);
+//        this.username = 'username';
+//        this.password = 'password';
     };
+    login = function(f: NgForm, callback) {
+//        console.log(f.value); 
+//        console.log(f.valid); 
+        if(!this.lockout){
+            if(f.valid && this.checkCredentials(f.value.username, f.value.password)){
+                this.username = f.value.username;
+                this.password = f.value.password;
+            } else {
+                var val = this.attemptMap[f.value.username];
+                (val) ? this.attemptMap[f.value.username] = val + 1 : this.attemptMap[f.value.username] = 1;
+                if(this.attemptMap[f.value.username] > 3) {
+                    this.lockout = true;    
+                    alert('Too many attempts, please wait 5 minutes.');
+                }
+                f.resetForm();
+            }
+            return f;
+        }
+    }
+    checkCredentials = function(u,p) {
+        return (u == 'admin' && p == 'pass');
+    }
 }
